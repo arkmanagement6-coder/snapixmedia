fbe1c035c04e8c9544000e877e23247d0a33ce72
Agenius Builder <builder@agenius.agency>
2026-06-27 12:00:58 +0530

    feat: add Google Tag Manager scripts to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Outfit } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({
@@ -32,8 +33,28 @@ export default function RootLayout({
       className={`${inter.variable} ${outfit.variable} scroll-smooth`}
     >
       <body className="bg-[#f8fafc] text-[#334155] font-sans selection:bg-indigo-600/10 selection:text-indigo-900 overflow-x-hidden min-h-screen flex flex-col">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5C59GFHH"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        
         {children}
+
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5C59GFHH');`}
+        </Script>
       </body>
     </html>
   );
 }
+
